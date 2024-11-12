b1f5f07a3bf0c6212a03600b875ea512166e32d8
Bryan Gardiner <bog@khumba.net>
2024-11-11 18:45:35 -0800

    Remove obsolete migration warning from default.nix.

--- default.nix
+++ default.nix
@@ -1,21 +1,12 @@
 { pkgs ? import <nixpkgs> {}
-, silenceWarning ? false
+
+# Obsolete parameter used during the migration to Sourcehut.
+# No need to pass it anymore.
+, silenceWarning ? null
 }:
 let
   inherit (pkgs) lib nix-gitignore python3 stdenv;
-
-  warning =
-    "This is a development build of nvd from git master.\n"
-    + "On or around Oct 19, 2024, nvd will be moving to Sourcehut.\n"
-    + "Please consider using the latest stable nvd release from nixpkgs,\n"
-    + "but if you wish to continue pulling the latest unstable pre-release\n"
-    + "code, you will need to update your URLs (and can do so now).\n"
-    + "See the following link for more info, thanks.\n\n"
-    + "https://gitlab.com/khumba/nvd/-/issues/19";
-
-  warn = if silenceWarning then lib.id else lib.warn warning;
 in
-warn (
 stdenv.mkDerivation {
   pname = "nvd";
   version = "0.2.5";
@@ -45,4 +36,3 @@ stdenv.mkDerivation {
     platforms = lib.platforms.all;
   };
 }
-)
